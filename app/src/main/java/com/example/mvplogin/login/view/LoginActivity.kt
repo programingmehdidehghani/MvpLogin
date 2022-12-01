ee6f4fa7dbc43954a3246586d2da7fe0e9039653
mehdi <dehghani_m@rocketmail.com>
2022-12-01 20:08:12 +0330

    make findview

--- app/src/main/java/com/example/mvplogin/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/mvplogin/login/view/LoginActivity.kt
@@ -2,11 +2,32 @@ package com.example.mvplogin.login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.mvplogin.R
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var et_text_id_login_activity : EditText
+    lateinit var et_text_pm_login_activity : EditText
+    lateinit var btn_login : Button
+    lateinit var tv_result_view_login_info : TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun findView(){
+        et_text_id_login_activity = findViewById(R.id.et_text_id_login_activity)
+        et_text_pm_login_activity = findViewById(R.id.et_text_pm_login_activity)
+        btn_login = findViewById(R.id.btn_login)
+        tv_result_view_login_info = findViewById(R.id.tv_result_view_login_info)
+
+        btn_login.setOnClickListener {
+            
+        }
     }
 }
