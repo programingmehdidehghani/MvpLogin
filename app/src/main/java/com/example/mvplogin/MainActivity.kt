553e7b7ced8c90042cba6c7e93c23e734ccb7168
mehdi <dehghani_m@rocketmail.com>
2022-12-01 19:38:39 +0330

    start make btn

--- app/src/main/java/com/example/mvplogin/MainActivity.kt
+++ app/src/main/java/com/example/mvplogin/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.mvplogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_login).setOnClickListener {
+            val intent = Intent()
+        }
     }
 }
