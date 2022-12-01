aa4ac1f7ceedf60c21f3ea06532ea3d4e9861c0d
mehdi <dehghani_m@rocketmail.com>
2022-12-01 19:45:14 +0330

    make package

---
+++ app/src/main/java/com/example/mvplogin/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvplogin.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvplogin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
